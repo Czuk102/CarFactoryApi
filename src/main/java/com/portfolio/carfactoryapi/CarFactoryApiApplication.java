7cb2f22ae26c87cf4900f30088bc92d9c09b2f60
Emilian Murawski <murawski.emilian@gmail.com>
2023-02-14 13:55:34 +0100

    Initial commit

---
+++ src/main/java/com/portfolio/carfactoryapi/CarFactoryApiApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.carfactoryapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarFactoryApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarFactoryApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/portfolio/carfactoryapi/CarFactoryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.carfactoryapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarFactoryApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
