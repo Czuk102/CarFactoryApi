4de0b0ea32ff8e8b7307b839b7dacf51d6dfa6b6
Emilian Murawski <murawski.emilian@gmail.com>
2023-02-17 00:28:58 +0100

    add GlobalExceptionHandler

---
+++ src/main/java/com/portfolio/carfactoryapi/controler/GlobalExceptionHandler.java
@@ -0,0 +1,32 @@
+package com.portfolio.carfactoryapi.controler;
+
+import com.portfolio.carfactoryapi.exception.CarNotFoudException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpServerErrorException;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(CarNotFoudException.class)
+    public ResponseEntity<String> handleCarNotFoundException(Exception e) {
+        return ResponseEntity.status(404).body("Object not found, message: " + e.getMessage());
+    }
+
+    @ExceptionHandler(HttpServerErrorException.class)
+    public ResponseEntity<String> handleServerError(Exception e) {
+        return ResponseEntity.status(502).body("Problem with server connection, message: " + e.getMessage());
+    }
+
+    @ExceptionHandler(HttpClientErrorException.BadRequest.class)
+    public ResponseEntity<String> handleBadRequest(Exception e) {
+        return ResponseEntity.status(400).body("Problem with server connection, message: " + e.getMessage());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleException(Exception e) {
+        return ResponseEntity.status(400).body("Unexpected exception occurred , message: " + e.getMessage());
+    }
+}
