cba08ffb2b052a09836fdd454d83aec29189e7ae
Emilian Murawski <murawski.emilian@gmail.com>
2023-07-18 20:49:54 +0200

    fix add repository annotation

--- src/main/java/com/portfolio/carfactoryapi/repository/CarFactoryRepository.java
+++ src/main/java/com/portfolio/carfactoryapi/repository/CarFactoryRepository.java
@@ -2,6 +2,8 @@ package com.portfolio.carfactoryapi.repository;
 
 import com.portfolio.carfactoryapi.model.Car;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CarFactoryRepository extends JpaRepository<Car,Long> {
 }
