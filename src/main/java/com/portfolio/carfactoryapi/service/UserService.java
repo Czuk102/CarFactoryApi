ed95552d181593bdd24e14435188592cbf4f0b43
Emilian Murawski <murawski.emilian@gmail.com>
2023-10-03 11:34:38 +0200

    feat add UserService responsible for registering and loading users

---
+++ src/main/java/com/portfolio/carfactoryapi/service/UserService.java
@@ -0,0 +1,39 @@
+package com.portfolio.carfactoryapi.service;
+
+import com.portfolio.carfactoryapi.exception.UserAlreadyExistsException;
+import com.portfolio.carfactoryapi.model.User;
+import com.portfolio.carfactoryapi.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService implements UserDetailsService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public UserDetails registerNewUser(User user) throws UserAlreadyExistsException {
+        if (isUserAlreadyExists(user.getUsername())) {
+            throw new UserAlreadyExistsException("User already exists: " + user.getUsername());
+        }
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        return userRepository.save(user);
+    }
+
+    private boolean isUserAlreadyExists(String userName) {
+        return userRepository.findUserByUsername(userName).isPresent();
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findUserByUsername(username).orElseThrow(() ->
+                new UsernameNotFoundException("User: " + username + " not found"));
+    }
+}
