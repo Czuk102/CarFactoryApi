46e68527e59610aebac4a6fb8bcf058c8d2ed8bf
Emilian Murawski <czuk102@gmail.com>
2024-02-09 21:32:17 +0100

    fix change role constraint to NotNull

--- src/main/java/com/portfolio/carfactoryapi/model/User.java
+++ src/main/java/com/portfolio/carfactoryapi/model/User.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -31,7 +32,7 @@ public class User implements UserDetails {
 
     @Enumerated(EnumType.STRING)
     
-    @NotEmpty
+    @NotNull
     private Role role;
 
 
